895db57e075628c248b5e0923c3d857ab58b917b
muqeeth-byte <abdulmuqeeth04@gmail.com>
2026-04-02 08:26:50 +0530

    Update Certifications.tsx

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -10,7 +10,7 @@ const certifications = [
     level: "Fundamental",
     skills: ["Azure Services", "Cloud Computing", "Microsoft Cloud"],
     
-    link: "https://learn.microsoft.com/en-us/certifications/azure-fundamentals/",
+    link: "https://drive.google.com/file/d/10n8-XPA_4VVBQD15kJ5L-JUoQKtgiBO7/view?usp=drive_link",
     featured: true,
     accent: "#0078D4",
   },
